e889405933cf0b59c25654c873a71a8b146b065f
VVDimitrov19 <VVDimitrov19@codingburgas.bg>
2023-05-04 12:44:58 +0300

    Finish Task

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,81 +1,64 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<map>
 using namespace std;
 
-// This function will swap characters for every permutation
-string SwapChar(string word, int i, int j)
-{
-    vector<string> swapIndex;
-    //word to list
-    for (int it = 0; it < word.size(); it++)
-    {
-        swapIndex.push_back(word.substr(it, 1));
-    }
-
-    //swap
-    string temp = swapIndex[i];
-    swapIndex[i] = swapIndex[j];
-    swapIndex[j] = temp;
-
-    //make word
-    string res = "";
-    for (int it = 0; it < swapIndex.size(); it++)
-        res += swapIndex[it];
-
-    return res;
-}
+// Map to store the mapping of digits to letters
+map<char, string> digitsMapping = { {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}, {'0', " "}, {'1', "@"} };
 
 // Recursive function to calculate permutations of the string
-void PermuteStringRec(string word, int currentIndex, vector<string>& result)
+void PermuteStringRec(string digits, string currentStr, int currentIndex, vector<string>& result)
 {
-    // Base case: add the permutation to the result vector
-    if (currentIndex == (word.size() - 1))
-    {
-        result.push_back(word);
-        return;
-    }
-    for (int i = currentIndex; i < word.size(); i++)
-    {
-        // Swap the characters at indices i and currentIndex
-        string swappedStr = SwapChar(word, currentIndex, i);
-        // Recursively call the function to calculate the permutations of the swapped string
-        PermuteStringRec(swappedStr, currentIndex + 1, result);
-    }
+	// Base case: add the permutation to the result vector
+	if (currentIndex == digits.size())
+	{
+		result.push_back(currentStr);
+		return;
+	}
+	// Get the letters corresponding to the current digit
+	string letters = digitsMapping[digits[currentIndex]];
+	// Loop through all the letters and recursively call the function to calculate the permutations of the remaining digits
+	for (int i = 0; i < letters.size(); i++)
+	{
+		PermuteStringRec(digits, currentStr + letters[i], currentIndex + 1, result);
+	}
 }
 
 // Function to calculate all possible permutations of the input string
-vector<string> PermuteWord(string word)
+vector<string> PermuteDigits(string digits)
 {
-    vector<string> result;
-    // Start calculating permutations from the start of the string
-    PermuteStringRec(word, 0, result);
-    return result;
+	vector<string> result;
+	PermuteStringRec(digits, "", 0, result);
+	return result;
 }
 
 // Function to print the elements of a vector as a string
 string PrintString(vector<string> str)
 {
-    string out = "[";
-    for (int i = 0; i < str.size(); i++)
-    {
-        out += str[i] + ", ";
-    }
-    out = out.substr(0, out.size() - 2) + "]";
-    return out;
+	string out = "[";
+	for (int i = 0; i < str.size(); i++)
+	{
+		out += str[i] + ", ";
+	}
+	out = out.substr(0, out.size() - 2) + "]";
+	return out;
 }
 
 // Driver code
 int main()
 {
-    vector<string> inputWord = { "ab", "bad", "abcd" };
-    for (int index = 0; index < inputWord.size(); index++)
-    {
-        vector<string> permutedWords = PermuteWord(inputWord[index]);
-        cout << index + 1 << ".\t Input string: '" << inputWord[index] << "'" << endl;
-        cout << "\t All possible permutations are: ";
-        cout << PrintString(permutedWords) << endl;
-        cout << string(100, '-') << endl;
-    }
-    return 0;
+	// Add mapping values for digits 0 and 1
+	digitsMapping['0'] = "+";
+	digitsMapping['1'] = " ";
+	// Calculate permutations for given numbers
+	vector<string> inputDigits = { "23", "138", "828" };
+	for (int index = 0; index < inputDigits.size(); index++)
+	{
+		vector<string> permutedStrings = PermuteDigits(inputDigits[index]);
+		cout << index + 1 << ".\t All letter combinations for '" << inputDigits[index] << "': ";
+		cout << PrintString(permutedStrings) << endl;
+		cout << string(100, '-') << endl;
+	}
+	return 0;
 }
